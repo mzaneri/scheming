0966392194cd6cab0c4135173953dd7b54a71bd7
mzaneri <michaelzaneri@gmail.com>
2019-04-08 15:37:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scheme
+
+Writing a Scheme in Haskell, chaning some bits to suit my own learning as I go.
---
+++ app/Main.hs
@@ -0,0 +1,51 @@
+module Main where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+            | List [LispVal]
+            | DottedList [LispVal] LispVal
+            | Number Integer
+            | String String
+            | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+            char '"'
+            x <- many (noneOf "\"")
+            char '"'
+            return $ String x            
+
+parseAtom :: Parser LispVal
+parseAtom = do
+                first <- letter <|> symbol
+                rest <- many (letter <|> digit <|> symbol <|>)
+                let atom = first:rest
+                return $ case atom of
+                            "#t" -> Bool True
+                            "#f" -> Bool False
+                             _   -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber :: liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "Failed to match " ++ show err
+    Right val -> "Found value " ++ show val
+
+main :: IO ()
+main = do
+    (expr:_) <- getArgs
+    putStrLn(readExpr expr)
